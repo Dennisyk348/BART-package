61a5438d9b122220b5010f478effc53bd87c5fb4
Dennisyk348 <157010776+Dennisyk348@users.noreply.github.com>
2024-05-28 00:00:58 +0800

    Add files via upload

--- loop.py
+++ loop.py
@@ -3,13 +3,14 @@ from sklearn.metrics import f1_score, roc_auc_score
 from tqdm import tqdm
 from parameter import dgp_params_dict_list_bartpy2, dataset_name
 from sim_util import run_one_dgp_iter
+import pandas as pd
 n_iter = 5
 def bartpy2(dataset_name,dgp_params_dict_list_bartpy2, metric=roc_auc_score,
          n_iter=n_iter):
     results = []
     for iter_num in tqdm(range(n_iter)):
         results+= run_one_dgp_iter(dataset_name, dgp_params_dict_list_bartpy2)
-        
-    return results
+    result= pd.DataFrame(results)
+    return result
 
 bartpy2(['breast_cancer'],dgp_params_dict_list_bartpy2,n_iter)
